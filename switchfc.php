9921f0e90714027b19f1249161e6dee4dd531b83
maurolopes <maurolopes@trt15.jus.br>
2025-03-15 14:28:06 +0000

    Novo funcionalidade

--- switchfc.php
+++ switchfc.php
@@ -1,31 +1,42 @@
 <?php
-class SwitchFC {
-    
-private string $name;
-private string $ip;
+class SwitchFC
+{
 
-/**
- * 
- */
-  public function __construct($name, $ip) {
-    $this->name = $name;
-    $this->ip = $ip;
-  }
+    private string $name;
+    private string $ip;
 
-  /**
-   * 
-   */
-  public function getName():string {
-    return $this->name;
-  } 
+    /**
+     * 
+     */
+    public function __construct($name, $ip)
+    {
+        $this->name = $name;
+        $this->ip = $ip;
+    }
 
-  /**
-   * 
-   */
-  public function getIP():string {
-    return $this->ip;
-  }
-  
+    /**
+     * 
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
+    /**
+     * 
+     */
+    public function getIP(): string
+    {
+        return $this->ip;
+    }
 
+    public function executeCmd(string cmd) {
+        return true;
+    }
+
+    public function __toString()
+    {
+        return sprintf("Name: %s, IP: %s", $this->name, $this->ip);
+
+    }
 }
